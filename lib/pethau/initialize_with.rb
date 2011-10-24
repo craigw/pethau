761b7700c01328254253be1f7bdf1202a3ab0da7
Craig R Webster <craig@barkingiguana.com>
2011-10-24 14:08:34 +0100

    Fix indentation

--- lib/pethau/initialize_with.rb
+++ lib/pethau/initialize_with.rb
@@ -6,16 +6,16 @@ module Pethau
 
     module ClassMethods
       def initialize_with *args
-	args.each do |attribute|
-	  attr_accessor attribute
-	  private attribute, "#{attribute}="
-	end
+        args.each do |attribute|
+          attr_accessor attribute
+          private attribute, "#{attribute}="
+        end
 
-	define_method :initialize do |*initial_args|
-	  args.each do |arg|
-	    send "#{arg}=", initial_args.shift
-	  end
-	end
+        define_method :initialize do |*initial_args|
+          args.each do |arg|
+            send "#{arg}=", initial_args.shift
+          end
+        end
       end
       private :initialize_with
     end
