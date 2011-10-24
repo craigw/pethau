296ca530b0d0f8ee7e14e3f85c061900db96d68f
Craig R Webster <craig@barkingiguana.com>
2011-10-24 15:44:39 +0100

    When the value is nil I actually want the default.

--- lib/pethau/default_value_of.rb
+++ lib/pethau/default_value_of.rb
@@ -2,24 +2,6 @@ module Pethau
   module DefaultValueOf
     def self.included into
       into.extend ClassMethods
-      into.send :include, InstanceMethods
-    end
-
-    module InstanceMethods
-      def value_set_cache
-        @value_set_for ||= {}
-      end
-      private :value_set_cache
-
-      def value_set_for attribute_name
-        value_set_cache[attribute_name] = true
-      end
-      private :value_set_for
-
-      def value_set_for? attribute_name
-        !!value_set_cache[attribute_name]
-      end
-      private :value_set_for?
     end
 
     module ClassMethods
@@ -27,41 +9,20 @@ module Pethau
         if default_value && block_given?
           raise "Only provide one of default value or builder" 
         end
-        default_value_setter_for attribute_name
-        default_value_getter_for attribute_name, default_value, &builder
-      end
-      private :default_value_of
 
-      def default_value_setter_for attribute_name
-        setter_name = "#{attribute_name}="
-        return unless method_defined? setter_name
-        setter_with_recorder = "#{attribute_name}_with_recorder="
-        setter_without_recorder = "#{attribute_name}_without_recorder="
-        alias_method setter_without_recorder, setter_name
-
-        define_method setter_with_recorder do |value|
-          send setter_without_recorder, value
-          value_set_for attribute_name
-        end
-        alias_method setter_name, setter_with_recorder
-      end
-      private :default_value_setter_for
-
-      def default_value_getter_for attribute_name, default_value, &builder
         getter_with_default = "#{attribute_name}_with_default"
         getter_without_default = "#{attribute_name}_without_default"
         alias_method getter_without_default, attribute_name
 
         define_method getter_with_default do
           original_value = send getter_without_default
-          return original_value if original_value
-          return original_value if value_set_for? attribute_name
+          return original_value unless original_value.nil?
           return builder.call if block_given?
           default_value
         end
         alias_method attribute_name, getter_with_default
       end
-      private :default_value_setter_for
+      private :default_value_of
     end
   end
 end
--- test/test_default_value_of.rb
+++ test/test_default_value_of.rb
@@ -1,27 +1,40 @@
 class TestDefaultValueOf < MiniTest::Unit::TestCase
-  def test_default_value_of_returns_default_value_when_not_set
+  def test_default_value_of_returns_default_value_when_block_given
     klass = Class.new do
       include Pethau::DefaultValueOf
-      attr_accessor :foo
+      attr_reader :foo
       default_value_of :foo do "HELLO, WORLD" end
     end
   
     instance = klass.new
-  
     assert_equal "HELLO, WORLD", instance.foo
   end
 
   def test_default_value_of_returns_default_value_when_literal_given
     klass = Class.new do
       include Pethau::DefaultValueOf
-      attr_accessor :foo
+      attr_reader :foo
       default_value_of :foo, "HELLO, WORLD"
     end
   
     instance = klass.new
-  
     assert_equal "HELLO, WORLD", instance.foo
   end
+
+  def test_default_value_of_returns_value_when_false_set
+    klass = Class.new do
+      include Pethau::DefaultValueOf
+      attr_accessor :foo
+      default_value_of :foo, true
+    end
+  
+    instance = klass.new false
+    instance.foo = false
+  
+    assert_equal false, instance.foo
+  end
+
+
   
   def test_default_value_of_returns_set_value_when_provided
     klass = Class.new do
@@ -35,26 +48,15 @@ class TestDefaultValueOf < MiniTest::Unit::TestCase
     assert_equal "GOODBYE!", instance.foo
   end
 
-  def test_default_value_of_returns_nil_when_nil_value_passed
+  def test_default_value_of_returns_default_when_nil_value_set
     klass = Class.new do
       include Pethau::DefaultValueOf
       attr_accessor :foo
-      default_value_of :foo do "Yo!" end
-    end
-
-    instance = klass.new
-    instance.foo = nil
-    assert_nil instance.foo
-  end
-
-  def test_default_value_of_works_when_no_setter_is_provided
-    klass = Class.new do
-      include Pethau::DefaultValueOf
-      attr_reader :foo
       default_value_of :foo, "Yo!"
     end
 
     instance = klass.new
+    instance.foo = nil
     assert_equal "Yo!", instance.foo
   end
 end
